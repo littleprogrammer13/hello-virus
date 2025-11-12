6b8201169ed9f4a7fa49262a7fbf7ddbbfef7911
littleprogrammer13 <buxybux0@gmail.com>
2025-11-12 19:23:29 -0300

    Delete pages directory

--- pages/api/status.js
+++
@@ -1,8 +0,0 @@
-// pages/api/status.js
-export default function handler(req, res) {
-  res.status(200).json({
-    status: "ok",
-    message: "Hello World!!! (safe server)",
-    timestamp: new Date().toISOString()
-  });
-}
